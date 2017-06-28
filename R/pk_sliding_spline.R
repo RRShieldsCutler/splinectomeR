f47651ab079e29bc3dbf3fcfb04c0ffb52fa77ca
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-06-28 11:45:16 -0500

    Fixed issues with function scope, added examples, cleaning up code

--- R/pk_sliding_spline.R
+++ R/pk_sliding_spline.R
@@ -18,24 +18,30 @@
 #' @param cases The column name defining the individual cases, e.g. patients.
 #' @param groups If more than two groups, the two groups to compare.
 #' @param set_spar Set the spar parameter for splines
-#' @param cut_low Remove data with fewer than __ points
+#' @param cut_sparse Remove sparse cases with fewer than __ data points (default 4)
+#' @param test_density Minimum density of cases in each group to report p-value (default 3)
 #' @param ints Number of x intervals over which to measure significance
 #' @param quiet Silence all text outputs
 #' @export
 #' @examples 
 #' result <- trendyspliner(data = ChickWeight, xvar = 'Time',
 #'              yvar = 'weight', category = 'Diet',
-#'              cases = 'Chick', group = '1')
+#'              cases = 'Chick', groups = '1,2')
 #' result$pval
 #' 
 
 sliding_spliner <- function(data = NA, xvar = NA, yvar = NA, category = NA,
                             cases = NA, groups = NA, set_spar = NULL,
-                            cut_low = NA, ints = 100, quiet = FALSE) {
+                            cut_sparse = 4, test_density = 3, ints = 100, quiet = FALSE) {
   
   require(dplyr)
   require(reshape2)
-
+  
+  reqs <- c(data, xvar, yvar, cases, category)
+  if (any(is.na(reqs))) {
+    stop('Missing required parameters. Run ?sliding_spliner to see help docs')
+  }
+  
   if (quiet == FALSE) {
     cat(paste('Running sliding spline test with', ints,
             'time points extrapolated from splines...\n'))
@@ -53,94 +59,101 @@ sliding_spliner <- function(data = NA, xvar = NA, yvar = NA, category = NA,
   # unit_number = 4  # min number of points needed per individual
   # set_spar = NULL  # default NULL
   
-  
+  df <- data
   
   if (is.na(groups)) {
     if (length(unique(df[, category])) > 2) {
       stop('More than two groups in category column. Define groups with "--groups=Name1,Name2"')
     }
-    v1 = unique(df[, category])[1]
-    v2 = unique(df[, category])[2]
+    v1 <- unique(df[, category])[1]
+    v2 <- unique(df[, category])[2]
   } else {
-    groups = as.character(groups)
-    v1 = strsplit(groups, ',')[[1]][1]
-    v2 = strsplit(groups, ',')[[1]][2]
+    groups <- as.character(groups)
+    v1 <- strsplit(groups, ',')[[1]][1]
+    v2 <- strsplit(groups, ',')[[1]][2]
   }
   
   # Make sure the df only contains data from categories tested
-  cat.vars = c(v1,v2)
-  df = df[df[, category] %in% cat.vars, ]
-  cases.tab = data.frame(table(df[, cases]))
-  cases.notsparse = cases.tab[cases.tab$Freq >= unit_number, ]
-  cases.keep = as.character(cases.notsparse$Var1)
-  df = df[df[, cases] %in% cases.keep, ]
+  cat.vars <- c(v1,v2)
+  df <- df[df[, category] %in% cat.vars, ]
+  cases.tab <- data.frame(table(df[, cases]))
+  cases.notsparse <- cases.tab[cases.tab$Freq >= cut_sparse, ]
+  cases.keep <- as.character(cases.notsparse$Var1)
+  df <- df[df[, cases] %in% cases.keep, ]
   
   # Get the range of the independent variable (e.g. time) to set spline limits
-  x.min = as.numeric(min(df[, xvar]))
-  x.max = as.numeric(max(df[, xvar]))
-  xrang = seq(x.min, x.max, by = ((x.max - x.min) / (ints - 1)))
+  x.min <- as.numeric(min(df[, xvar]))
+  x.max <- as.numeric(max(df[, xvar]))
+  xrang <- seq(x.min, x.max, by = ((x.max - x.min) / (ints - 1)))
   
   # Save the group labels for each individual/unit
-  df.groups = df %>% distinct_(cases, .keep_all = T)
-  df.groups = df.groups %>% select_(cases, category)
+  df.groups <- df %>% 
+              distinct_(cases, .keep_all = T) %>% 
+              select_(cases, category)
   
   # Generate splines for each individual
-  spl.table = setNames(data.frame(xrang), c('x'))
+  spl.table <- setNames(data.frame(xrang), c('x'))
   for (i in cases.keep) {
-    unit.df = subset(df, df[, cases]==i)
-    unit.spl = with(unit.df,
+    unit.df <- subset(df, df[, cases]==i)
+    unit.spl <- with(unit.df,
                     smooth.spline(x=unit.df[, xvar], y=unit.df[, yvar],
                                   spar = set_spar))
-    xrang.i = subset(xrang, xrang >= min(unit.spl$x) & xrang <= max(unit.spl$x))
-    unit.spl.f = data.frame(predict(unit.spl, xrang.i))
-    colnames(unit.spl.f) = c('x', i)
-    spl.table = merge(spl.table, unit.spl.f, by = 'x', all = T)
+    xrang.i <- subset(xrang, xrang >= min(unit.spl$x) & xrang <= max(unit.spl$x))
+    unit.spl.f <- data.frame(predict(unit.spl, xrang.i))
+    colnames(unit.spl.f) <- c('x', i)
+    spl.table <- merge(spl.table, unit.spl.f, by = 'x', all = T)
   }
   
   # Prepare the spline table for statistical testing
-  spl.table.p = tibble::column_to_rownames(df = spl.table, var = 'x')
-  spl.table.p = as.data.frame(t(spl.table.p))
-  spl.table.p = tibble::rownames_to_column(spl.table.p, var = cases)
-  spl.table.p = merge(spl.table.p, df.groups, by = cases, all = T)
+  spl.table.p <- tibble::column_to_rownames(df = spl.table, var = 'x')
+  spl.table.p <- as.data.frame(t(spl.table.p))
+  spl.table.p <- tibble::rownames_to_column(spl.table.p, var = cases)
+  spl.table.p <- merge(spl.table.p, df.groups, by = cases, all = T)
   
-  n = xrang[3]
-  spline.table = spl.table.p
+  spline.table <- spl.table.p
   # Define the non-parametric test function
-  mann_whitney_per_bit = function(spline.table, n) {
-    n = as.character(n)
-    x.pick = c(cases, n, category)
-    x.table = spline.table[, x.pick]
-    x.table = x.table[!is.na(x.table[, n]), ]
-    x.table = droplevels(x.table)
-    cat.freq = data.frame(table(x.table[, category]))
-    if (cat.freq$Freq[1] >= sparsity & cat.freq$Freq[2] >= sparsity) {
-      mw = wilcox.test(x.table[, n] ~ x.table[, category])
-      pval = mw$p.value
-      num.pts = cat.freq$Freq[1] + cat.freq$Freq[2]
+  mann_whitney_per_bit <- function(spline.table, n) {
+    n <- as.character(n)
+    x.pick <- c(cases, n, category)
+    x.table <- spline.table[, x.pick]
+    x.table <- x.table[!is.na(x.table[, n]), ]
+    x.table <- droplevels(x.table)
+    cat.freq <- data.frame(table(x.table[, category]))
+    if (cat.freq$Freq[1] >= test_density & cat.freq$Freq[2] >= test_density) {
+      mw <- wilcox.test(x.table[, n] ~ x.table[, category])
+      pval <- mw$p.value
+      num.pts <- cat.freq$Freq[1] + cat.freq$Freq[2]
       return(c(pval, n, num.pts))
     } else {}
   }
   
   # Run the stats test on each step of the spline on each data unit
-  pvals.list = list()
-  pval.x = list()
-  pval.num.pts = list()  # Save the number of total data points in each comparison
+  pvals.list <- list()
+  pval.x <- list()
+  pval.num.pts <- list()  # Save the number of total data points in each comparison
   for (n in xrang) {
-    pval = mann_whitney_per_bit(spl.table.p, n)
-    pvals.list = append(pvals.list, pval[1])
-    pval.x = append(pval.x, pval[2])
-    pval.num.pts = append(pval.num.pts, pval[3])
+    pval <- mann_whitney_per_bit(spl.table.p, n)
+    pvals.list <- append(pvals.list, pval[1])
+    pval.x <- append(pval.x, pval[2])
+    pval.num.pts <- append(pval.num.pts, pval[3])
   }
-  pval.df = do.call(rbind, Map(data.frame,
+  pval.df <- do.call(rbind, Map(data.frame,
                                x.series=as.numeric(pval.x),
                                p.value=as.numeric(pvals.list),
                                N=as.numeric(pval.num.pts)))
-  colnames(pval.df) = c(xvar, 'p-value', 'number_of_observations')
-  result <- list('pval_table' = pval.df, 'spline_table' = spl.table)
+  colnames(pval.df) <- c(xvar, 'p_value', 'number_of_observations')
+  
+  plot.spline.data <- melt(data = spl.table, id.vars = 'x')
+  colnames(plot.spline.data) <- c('x', cases, 'value')
+  plot.spline.data <- merge(plot.spline.data, df.groups, by = cases, all = T)
+  colnames(plot.spline.data) <- c('UNIT', 'x', 'value', 'category')
+  
+  result <- list('pval_table' = pval.df, 'spline_table' = spl.table,
+                 'spline_longform' = plot.spline.data)
   return(result)
 }
 
-#' @title Sliding spliner plot splines
+#' @title Plot group splines
 #' @description Plot the individual splines grouped by two categories of interest
 #' @rdname sliding_spliner.plot.splines
 #' @param data The dataframe of imputed points generated from splines
@@ -148,22 +161,21 @@ sliding_spliner <- function(data = NA, xvar = NA, yvar = NA, category = NA,
 #' @param y The dependent variable; is continuous, e.g. temperature.
 #' @param cases The column name defining the individual cases, e.g. patients.
 #' @export
-#' 
+#' @examples sliding_spliner.plot.splines(result$spline_longform,
+#'  category = 'Diet', xvar = 'Time', yvar = 'weight')
+#'
 
   # Plot the splines
-sliding_spliner.plot.splines <- function(data_df, cases = cases,
+sliding_spliner.plot.splines <- function(plot.spline.data, category = category,
                                          xvar = xvar, yvar = yvar) {
   require(ggplot2)
-  spl.p <- data_df
-  plot.spline.data = melt(data = spl.p, id.vars = 'x')
-  colnames(plot.spline.data) = c('x', cases, 'value')
-  plot.spline.data = merge(plot.spline.data, df.groups, by = cases, all = T)
-  colnames(plot.spline.data) = c('UNIT', 'x', 'value', 'category')
-  ggplot(plot.spline.data, aes(x=x, y=value, group=UNIT,
-    color=as.character(category))) + geom_line(na.rm = T) +
-    scale_color_manual(name=category, values = c("#0072B2","#D55E00")) +
+  ggplot(plot.spline.data, aes(x = x, y = value, group = UNIT,
+    color = as.character(category))) + geom_line(na.rm = T) +
+    scale_color_manual(name = category, values = c("#0072B2","#D55E00")) +
     xlab(xvar) + ylab(yvar) +
-    theme(legend.position='right')
+    theme_bw() + theme(legend.position='right',
+                       plot.background = element_rect(color = 'white'),
+                       panel.grid = element_blank())
 }
 
 #' @title Plot sliding spliner p-values
@@ -173,22 +185,26 @@ sliding_spliner.plot.splines <- function(data_df, cases = cases,
 #' @param x The independent variable; is continuous, e.g. time.
 #' @param cases The column name defining the individual cases, e.g. patients.
 #' @export
-#' 
+#' @examples sliding_spliner.plot.pvals(result$pval_table,
+#'  cases = 'Chick')
+#'
 
 
 # Plot the p-values as function of the independent variable (e.g. time)
 # Scale the size of the line and points according to the number of observations
-sliding_spliner.plot.pvals <- function(data_df, cases = cases,
-                                       xvar = xvar) {
-  require(ggplot2)
-  pval.p <- data_df
-  .norm_range <- function(x){(x-min(x)) / (max(x)-min(x))}
-  pval.p$N.norm = norm_range(x = pval.p$N)
-  ggplot(pval.p, aes(x=x.series, y=p.value)) + geom_line() +
+sliding_spliner.plot.pvals <- function(data, cases = cases) {
+  library(ggplot2)
+  .norm_range <- function(x) {(x-min(x)) / (max(x)-min(x))}
+  pval.p <- data
+  xvar <- names(pval.p)[1]
+  pval.p$N.norm <- .norm_range(x = pval.p[,3])
+  ggplot(pval.p, aes(x=pval.p[,1], y=p_value)) + geom_line() +
     geom_point(shape = 20, size = (pval.p$N.norm * 2))  +
     geom_hline(aes(yintercept = 0.05), linetype='dashed') +
     xlab(xvar) + ylab('Mann-Whitney p-value') +
     scale_y_continuous(trans = scales::log10_trans()) +
-    theme(legend.position='none') #+ ylim(0,maxp)
+    theme_bw() + theme(legend.position='none',
+                       plot.background = element_rect(color = 'white'),
+                       panel.grid = element_blank())
 }
 
